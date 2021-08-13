d7e25b54777d3c3dcdb3fef879eb88aba5753d09
Steven Bruno <sbruno636@gmail.com>
2021-08-13 12:35:51 -0400

    update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        "-10": "-10",
+      },
+    },
+    fontFamily: {
+      serif: ["Cormorant", "ui-serif", "Georgia"],
+      sans: ["SF Pro Text", "ui-sans-serif", "system-ui"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
