168148b5bc8ed7e2a17d5f6e25d16b45fe0d3b97
Steven Bruno <sbruno636@gmail.com>
2021-08-12 22:01:55 -0400

    index page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,31 @@
-import * as React from "react"
-
+import * as React from "react";
+import { Helmet } from "react-helmet";
+import Project from "../components/Project.js";
 
 const IndexPage = () => {
   return (
-    <div class="pt-4">
-      <nav class="px-4 mb-4">
-        <p>Steven Bruno</p>
-        <p>sbruno636@gmail.com</p>
+    <div className="pt-4 text-gray-600">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Steven Bruno</title>
+        <link rel="canonical" href="https:stevenbruno.design" />
+        <html lang="en" />
+      </Helmet>
+      <nav className="px-4 mb-20 flex flex-col text-center">
+        <p className="text-xl font-serif tracking-wide">Steven Bruno</p>
+        <p className="text-sm uppercase">sbruno636@gmail.com</p>
         <p>social links</p>
       </nav>
-      <main class="px-4">
-        <p class="text-5xl">Hi, I'm Steven</p>
-        <p> I'm a product designer based in New York City. Currently, I'm designing technology for restaurants at Toast.</p>
+      <main className="px-4">
+        <h1 className="text-4xl font-light font-serif mb-4">Hi, I'm Steven</h1>
+        <p className="mb-40">
+          I'm a product designer based in New York City. Currently, I'm
+          designing technology for restaurants at Toast.
+        </p>
+        <Project />
       </main>
-    </div>)
-}
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
