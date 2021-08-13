f881592c230261ed8db60b16571e7bd8f9de93d5
Steven Bruno <sbruno636@gmail.com>
2021-08-12 22:00:50 -0400

    update siteURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.stevenbruno.design",
     title: "StevenBrunoDesign",
   },
   plugins: [
