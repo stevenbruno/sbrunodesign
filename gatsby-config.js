93cc957cb3fcadac7ddf6c86aaedb7a3e32c45f9
Steven Bruno <sbruno636@gmail.com>
2021-08-18 15:37:02 -0400

    attempt to resolve the build path error

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: `./src/images`,
       },
       __key: "images",
     },
