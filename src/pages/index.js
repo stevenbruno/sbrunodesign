03c8efafe233415c872e08a67d17d2a752a869e4
Steven Bruno <sbruno636@gmail.com>
2021-08-20 20:58:04 -0400

    style various elements, handle email click event

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import * as React from "react";
+import { useState } from "react";
 import { Helmet } from "react-helmet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faDev } from "@fortawesome/free-brands-svg-icons";
 import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
 import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    navigator.clipboard.writeText("sbruno636@gmail.com");
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 1600);
+  };
   return (
     <>
       <StaticImage
@@ -15,7 +25,7 @@ const IndexPage = () => {
         className="-z-10 h-supertall"
         style={{ position: "absolute" }}
       />
-      <div className="p-6 sm:p-10 text-gray-700 text-xl">
+      <div className="p-6 sm:p-10 sm:text-lg text-gray-600">
         <Helmet>
           <meta charSet="utf-8" />
           <title>Steven Bruno</title>
@@ -31,77 +41,92 @@ const IndexPage = () => {
             rel="stylesheet"
           />
         </Helmet>
-        <nav className="mb-20 lg:mb-32 flex flex-wrap border-b-2 border-gray-300 pb-4 justify-between items-center gap-x-6 space-y-1">
-          <div className="flex flex-col mb-2">
-            <p className="text-2xl font-serif uppercase text-gray-700">
-              Steven Bruno
-            </p>
-            <p className="text-sm font-serif tracking-wider uppercase">
-              sbruno636@gmail.com
-            </p>
-          </div>
-          <div className="flex space-x-3 text-gray-400 text-sm md:text-xs">
-            <FontAwesomeIcon icon={faLinkedin} size="3x" />
-            <FontAwesomeIcon icon={faDev} size="3x" />
-            <FontAwesomeIcon icon={faGithubSquare} size="3x" />
-          </div>
-        </nav>
-        <div className="mx-auto max-w-screen-lg lg:pl-8 mb-48">
-          <h1 className="text-5xl sm:text-7xl font-serif mb-4 text-gray-800">
-            Hi, I'm Steven
+        <header className="flex flex-col items-center mb-48 pt-24 lg:pr-16">
+          <h1 className="text-5xl sm:text-sbxl font-serif mb-4 text-gray-700 text-center font-semibold">
+            Steven Bruno
           </h1>
-          <p className="mb-52 leading-relaxed max-w-xl">
-            I'm a product designer based in New York City. Currently, I'm
-            designing technology for restaurants at Toast.
+          <p className="mb-24 text-center max-w-xs tracking-wide px-4 sm:max-w-none">
+            Steven is a{" "}
+            <span className="font-semibold text-gray-600">
+              product designer
+            </span>{" "}
+            at Toast in New York City
           </p>
-          <div className="flex flex-wrap justify-start sm:justify-start gap-16 lg:gap-24">
-            <a href="https://stevenbruno.notion.site/Payroll-Redesign-8dca753b8ac24311baa079ace9a35e15">
-              <div
-                className={
-                  "py-16 px-2 min-h-72 min-w-72 max-w-xs sm:max-w-none sm:w-96 sm:h-96 rounded-lg bg-gradient-to-r from-yellow-50 to-yellow-100"
-                }
+          <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4 mx-auto">
+            <div className="flex space-x-3 text-gray-400 text-sm md:text-xs text-center max-w-max">
+              <a href="https://www.linkedin.com/in/stevenjbruno/">
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
+              </a>
+              <a href="https://dev.to/stevenbruno">
+                <FontAwesomeIcon icon={faDev} size="3x" />
+              </a>
+              <a href="https://github.com/stevenbruno">
+                <FontAwesomeIcon icon={faGithubSquare} size="3x" />
+              </a>
+            </div>
+            <div className="flex flex-col items-center">
+              <button
+                className="mb-2 bg-emailbg text-emailblue hover:text-blue-900 active:bg-blue-300 px-4 py-2 rounded-full text-sm font-medium"
+                onClick={handleClick}
               >
-                <StaticImage
-                  src="../images/payrolldash.png"
-                  alt="Payroll Dashboard Mockup"
-                  placeholder="blurred"
-                />
-              </div>
-            </a>
-            <a href="https://stevenbruno.notion.site/MyToast-24edb88d60264f21a54063c2899ea3fa">
-              <div
-                className={
-                  "min-h-72 min-w-72 max-w-xs sm:max-w-none sm:w-96 sm:h-96 rounded-lg bg-gradient-to-r from-purple-50 to-pink-100"
-                }
-              >
-                {/* <StaticImage
+                sbruno636@gmail.com <FontAwesomeIcon icon={faCopy} />
+              </button>
+              <p className={`text-sm ${clicked ? "visible" : "invisible"}`}>
+                &#10004; Copied to clipboard
+              </p>
+            </div>
+          </div>
+        </header>
+        <div className="flex flex-wrap max-w-4xl mx-auto sm:items-start md:items-center gap-16 lg:gap-24 mb-40 md:mb-56">
+          <a href="https://stevenbruno.notion.site/Payroll-Redesign-8dca753b8ac24311baa079ace9a35e15">
+            <div
+              className={
+                "py-16 px-2 min-h-72 min-w-72 max-w-xs sm:max-w-none sm:w-96 sm:h-96 rounded-lg bg-gradient-to-r from-yellow-50 to-yellow-100"
+              }
+            >
+              <StaticImage
+                src="../images/payrolldash.png"
+                alt="Payroll Dashboard Mockup"
+                placeholder="blurred"
+              />
+            </div>
+          </a>
+          <a href="https://stevenbruno.notion.site/MyToast-24edb88d60264f21a54063c2899ea3fa">
+            <div
+              className={
+                "min-h-72 min-w-72 max-w-xs sm:max-w-none sm:w-96 sm:h-96 rounded-lg bg-gradient-to-r from-purple-50 to-pink-100"
+              }
+            >
+              {/* <StaticImage
                   src="../images/payrolldash.png"
                   alt="MyToast App Mockup"
                   placeholder="blurred"
                 /> */}
-              </div>
-            </a>
-            <a href="https://stevenbruno.notion.site/Toast-Tips-Manager-3336ba38ec2e4baf82ba7677b1d13bad">
-              <div
-                className={
-                  "min-h-72 min-w-72 max-w-xs sm:max-w-none sm:w-96 sm:h-96 rounded-lg bg-gradient-to-r from-blue-100 to-indigo-200"
-                }
-              >
-                {/* <StaticImage
+            </div>
+          </a>
+          <a href="https://stevenbruno.notion.site/Toast-Tips-Manager-3336ba38ec2e4baf82ba7677b1d13bad">
+            <div
+              className={
+                "min-h-72 min-w-72 max-w-xs sm:max-w-none sm:w-96 sm:h-96 rounded-lg bg-gradient-to-r from-blue-100 to-indigo-200"
+              }
+            >
+              {/* <StaticImage
                   src="../images/payrolldash.png"
                   alt="Tips Manager Mockup"
                   placeholder="blurred"
                 /> */}
-              </div>
-            </a>
-          </div>
+            </div>
+          </a>
         </div>
         <div className="mx-auto max-w-screen-sm">
           <h2 className="text-4xl font-serif text-gray-900 mb-6">About me</h2>
           <p className="mb-4">
             I grew up in Los Angeles then moved to Chicago to study Civil
-            Engineering and Architectural Design at Northwestern University. My
-            grand visions of becoming an architect took a backseat once I
+            Engineering and Architectural Design at{" "}
+            <span className="font-semibold text-gray-700">
+              Northwestern University
+            </span>
+            . My grand visions of becoming an architect took a backseat once I
             discovered web design. The ability to imagine and build your vision
             in three days as opposed to three years was too enticing to ignore.
           </p>
@@ -112,19 +137,25 @@ const IndexPage = () => {
             designing software for restaurants ever since.
           </p>
           <p className="mb-4">
-            I’m a UX generalist comfortable working across the design ecosystem.
-            I am equally at home leading research initiatives and facilitating
-            design workshops as I am building prototypes and committing
-            production front-end code.
+            I’m a{" "}
+            <span className="font-semibold text-gray-700">UX generalist</span>{" "}
+            comfortable working across the design ecosystem. I am equally at
+            home leading research initiatives and facilitating design workshops
+            as I am building prototypes and committing production front-end
+            code.
           </p>
-          <p className="mb-4">
+          <p className="mb-24">
             In my free time, I enjoy running, reading, soccer, chess, and trying
             new restaurants. Lately, I’ve been diving deep into climate and
             crypto.
           </p>
-          <p className="mb-4">
-            I designed and built this website with Figma, React, Gatsby, and
-            TailwindCSS.
+          <p className="mb-1 text-gray-500 font-light italic">
+            I designed and developed this website with Figma, React, Gatsby, and
+            TailwindCSS. Let me know if you find any bugs or have general
+            feedback. <span className="not-italic">&#128578;</span>
+          </p>
+          <p className="mb-4 text-gray-500 font-light italic">
+            sbruno636@gmail.com
           </p>
         </div>
       </div>
